067f77bed68414de2c0e7bd31112e033b8a5d26e
AndresRedondo87 <66962177+AndresRedondo87@users.noreply.github.com>
2020-09-13 19:30:23 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace File_WerkTage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // #1 - Dateiname / Pfad
+            string dateiName = @"Testdaten\Werktage September.txt";
+
+            // #2 - Daten (Text oder Binär?)
+            string[] werktageImSeptember = FindeWerktageIn(09, 2016);
+
+            // #3 - Passende Methode von der Klasse File
+            File.WriteAllLines(dateiName, werktageImSeptember);
+
+            string[] dateiInhalt = File.ReadAllLines(@"Testdaten\Werktage mit Umlauten.txt", Encoding.Default);
+            foreach (var datum in dateiInhalt)
+            {
+                Console.WriteLine(datum);
+            }
+
+            Console.ReadLine();
+        }
+
+        static string[] FindeWerktageIn(int monat, int jahr)
+        {
+            var werktage = new List<string>();
+            DateTime ersterTag = new DateTime(jahr, monat, 1);
+            int anzahlTageImMonate = DateTime.DaysInMonth(jahr, monat);
+
+            for (int i = 0; i < anzahlTageImMonate; i++)
+            {
+                DateTime tag = ersterTag.AddDays(i);
+
+                // Samstag und Sonntag sind natürlich keine Werktage
+                if ((tag.DayOfWeek != DayOfWeek.Sunday)
+                    && (tag.DayOfWeek != DayOfWeek.Saturday))
+                {
+                    werktage.Add(tag.ToLongDateString());
+                }
+            }
+
+            return werktage.ToArray();
+        }
+
+    }
+}
