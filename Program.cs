595ce4c8625781542a4273d0b5b022c12e0d678e
AndresRedondo87 <66962177+AndresRedondo87@users.noreply.github.com>
2020-09-13 19:52:25 +0200

    Video 107 - Daten in Dateien schreiben / aus Dateien Lesen

--- Program.cs
+++ Program.cs
@@ -9,20 +9,38 @@ namespace File_WerkTage
     {
         static void Main(string[] args)
         {
-            // #1 - Dateiname / Pfad
-            string dateiName = @"Testdaten\Werktage September.txt";
+            // #1 - Dateiname / Pfad        //am besten immer mit Verbatim String definieren
+            string dateiName = @"Testdaten\Werktage September.txt"; 
+            //PFAD MUSS EXISTIEREN!! 
+            //den Ordner Testdaten muss erst da sein, sonst haben wir exceptions!!
 
             // #2 - Daten (Text oder Binär?)
-            string[] werktageImSeptember = FindeWerktageIn(09, 2016);
+            string[] werktageImSeptember = FindeWerktageIn(09, 2020);   //aktualisiert 2020 anstatt 2016
 
             // #3 - Passende Methode von der Klasse File
             File.WriteAllLines(dateiName, werktageImSeptember);
-
-            string[] dateiInhalt = File.ReadAllLines(@"Testdaten\Werktage mit Umlauten.txt", Encoding.Default);
-            foreach (var datum in dateiInhalt)
+            ///WriteAllLBytes - schreibt bytes weg
+            ///WriteAllLines - schreibt strings weg kann mehrere Strings als parameter bekommen
+            ///WriteAllText - schreibt nur ein string. wir bekommen nur ein string parameter     
+            ///WriteAll - erzeugt/überschreibt Dateien
+            ///AppendAll - fügt an bestehende Dateien an
+            ///
+            ///Pfade hier auch IMMER ALLE IN VERBATIM!!
+            ///
+            //string[] dateiInhalt = File.ReadAllLines(@"Testdaten\Werktage mit Umlauten.txt"); probleme mit Umlaute
+            //string[] dateiInhalt10 = File.ReadAllLines(@"Testdaten\Werktage mit Umlauten.txt", Encoding.Default);       // Auch Probleme mit umlaute
+            string[] dateiInhalt2 = File.ReadAllLines(@"Testdaten\Werktage mit Umlauten.txt", Encoding.ASCII);          // auch Probleme "??"
+            string[] dateiInhalt3 = File.ReadAllLines(@"Testdaten\Werktage mit Umlauten.txt", Encoding.UTF8);           // DIESE HAT UMLAUTE richtig gezeigt!
+            foreach (var datum in dateiInhalt2)
             {
                 Console.WriteLine(datum);
             }
+            Console.WriteLine();
+            foreach (var datum in dateiInhalt3)
+            {
+                Console.WriteLine(datum);
+            }
+
 
             Console.ReadLine();
         }
